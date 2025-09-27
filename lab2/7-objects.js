468b94aecd607a31b5f496348c759259524b7454
Jutsu78 <loshataraev@gmail.com>
2025-09-27 22:53:33 +0300

    task 7 "Objects" is done

---
+++ lab2/7-objects.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+const fn = () => {
+    const obj1 = {name: "Oleksii"};
+    let obj2 = {name: "Dima"};
+
+
+    obj1.name = "Lena";
+    obj2.name = "Sasha";
+return {obj1, obj2};
+    
+
+};
