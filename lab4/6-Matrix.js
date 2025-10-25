d835324f0a7d1e1128bdbd5ff90961e0538c686d
Jutsu78 <loshataraev@gmail.com>
2025-10-25 19:42:03 +0300

    task 6 is done

---
+++ lab4/6-Matrix.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+const max = (matrix) => {
+ let value = matrix[0][0];
+ for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+     for (let j = 0; j < matrix[i].length; j++) {
+        const cell = row[j];
+        if (value < cell) value = cell;
+        }
+    }
+    return value;
+const m = max([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]);
+
+console.log(m); // 9
+};
